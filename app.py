eca1030c68a06ec7903a306972abd20f02aa3a21
ananyabangalore1 <137497480+ananyabangalore1@users.noreply.github.com>
2024-11-06 15:48:48 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import openai
 import streamlit as st
 from creds import openai_api_key
 
-openai.api_key=openai_api_key
+openai.api_key="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"
 
 # Custom CSS to set colors
 st.markdown(
